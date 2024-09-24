53acf7a6f9c539deaffb62373d3b26313a1438b1
Maurits Meester <maurits@e-mmer.nl>
2024-09-24 21:19:49 +0200

    feat(socials): style the socials and newsletter

--- components/content/NewsletterSignup.vue
+++ components/content/NewsletterSignup.vue
@@ -1,32 +1,27 @@
 <template>
-  <div class="max-w-2xl mx-auto">
+  <div class="max-w-2xl mx-auto my-20">
     <div id="mc_embed_signup">
       <form
         action="https://broekerhuis.us21.list-manage.com/subscribe/post?u=1c8c2f6dd5d93679770c29b9b&amp;id=5c058a5ec2&amp;f_id=0007bee6f0"
-        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_self"
-        novalidate>
-        <div id="mc_embed_signup_scroll">
-          <h2
-            class="mt-2 text-pretty text-xl font-medium tracking-tighter text-swamp-green-900 data-[dark]:text-white sm:text-3xl">
-            <ContentSlot :use="$slots.default" unwrap="p" />
-          </h2>
+        method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_self">
+        <h2
+          class="my-2 text-pretty text-xl font-medium tracking-tighter text-swamp-green-900 data-[dark]:text-white sm:text-3xl">
+          <ContentSlot :use="$slots.default" unwrap="p" />
+        </h2>
 
-          <UFormGroup label="E-mailadres *" name="EMAIL">
+        <div class="flex flex-nowrap align-bottom gap-4">
+          <UFormGroup label="E-mailadres *" name="EMAIL" class="grow">
             <UInput type="email" />
           </UFormGroup>
-
-          <UButton type="submit">
-            Inschrijven
-          </UButton>
-
-          <div id="mce-responses" class="clear">
-            <div class="response" id="mce-error-response" style="display: none;"></div>
-            <div class="response" id="mce-success-response" style="display: none;"></div>
+          <div class="flex flex-col justify-end">
+            <UButton type="submit">
+              Inschrijven
+            </UButton>
           </div>
-          <div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text"
-              name="b_1c8c2f6dd5d93679770c29b9b_5c058a5ec2" tabindex="-1" value=""></div>
-
         </div>
+
+        <div aria-hidden="true" style="position: absolute; left: -5000px;"><input type="text"
+            name="b_1c8c2f6dd5d93679770c29b9b_5c058a5ec2" tabindex="-1" value=""></div>
       </form>
     </div>
   </div>
---
+++ components/content/ProseH3.vue
@@ -0,0 +1,5 @@
+<template>
+  <h3 class="text-xl font-medium tracking-tight text-gray-950 sm:text-2xl mb-4">
+    <ContentSlot :use="$slots.default" unwrap="p" />
+  </h3>
+</template>
